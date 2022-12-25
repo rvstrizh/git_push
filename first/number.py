31e9a27f6e61803f850fa068b28ce2b7465bd0e1
rvstrizh <rvstrizh@gmail.com>
2022-12-25 18:38:14 +0300

    добавлен strings.py удален текст

--- first/number.py
+++ first/number.py
@@ -1,10 +1,12 @@
 def plus(a, b):
     return a+b
-
-text = 'new text'
-
 def minus(a, b):
     return a - b
+
+def mul(a, b):
+    return a*b
+
 if __name__ == '__main__':
     print(plus(2, 2))
     print(minus(4, 2))
+    print(mul(3, 2))
